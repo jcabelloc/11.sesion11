e3d0fa521a3a0247c414c0ed1e6896e61604fb50
Juan Cabello <jcabelloc@itana.pe>
2024-11-04 20:05:06 -0500

    Uso de token para reinicio de password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,14 @@
 const Usuario = require('../models/usuario')
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
+
 
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 
 // AQUI_SE_PONE_EL_API_KEY
-const APIKEY = '';
+const APIKEY = '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';
 
 const transporter = nodemailer.createTransport(
   sendgridTransport({
@@ -143,3 +145,99 @@ exports.getReinicio = (req, res, next) => {
     mensajeError: mensaje
   });
 };
+
+
+
+exports.postReinicio = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reinicio');
+    }
+    const token = buffer.toString('hex');
+    Usuario.findOne({ email: req.body.email })
+      .then(usuario => {
+        if (!usuario) {
+          req.flash('error', 'No se encontro usuario con dicho email');
+          return res.redirect('/reinicio');
+        }
+        usuario.tokenReinicio = token;
+        usuario.expiracionTokenReinicio = Date.now() + 3600000; // + 1 hora
+        return usuario.save();
+      })
+      .then(result => {
+        res.redirect('/');
+        transporter.sendMail({
+          to: req.body.email,
+          from: 'jcabelloc@itana.pe',
+          subject: 'Reinicio de Password',
+          html: `
+            <p>Tu has solicitado un reinicio de password</p>
+            <p>Click aqui <a href="http://localhost:3000/reinicio/` + token + `">link</a> para establecer una nuevo password.</p>
+          `
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+};
+
+
+
+
+exports.getNuevoPassword = (req, res, next) => {
+  const token = req.params.token;
+  console.log(token)
+  Usuario.findOne({ tokenReinicio: token, expiracionTokenReinicio: { $gt: Date.now() } })
+    .then(usuario => {
+      console.log(usuario)
+      let mensaje = req.flash('error');
+      if (mensaje.length > 0) {
+        mensaje = mensaje[0];
+      } else {
+        mensaje = null;
+      }
+      res.render('auth/nuevo-password', {
+        path: '/nuevo-password',
+        titulo: 'Nuevo Password',
+        mensajeError: mensaje,
+        idUsuario: usuario._id.toString(),
+        tokenPassword: token
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+
+exports.postNuevoPassword = (req, res, next) => {
+  const nuevoPassword = req.body.password;
+  const idUsuario = req.body.idUsuario;
+  const tokenPassword = req.body.tokenPassword;
+  let usuarioParaActualizar;
+
+  Usuario.findOne({
+    tokenReinicio: tokenPassword,
+    expiracionTokenReinicio: { $gt: Date.now() },
+    _id: idUsuario
+  })
+    .then(usuario => {
+      usuarioParaActualizar = usuario;
+      return bcrypt.hash(nuevoPassword, 12);
+    })
+    .then(hashedPassword => {
+      usuarioParaActualizar.password = hashedPassword;
+      usuarioParaActualizar.tokenReinicio = undefined;
+      usuarioParaActualizar.expiracionTokenReinicio = undefined;
+      return usuarioParaActualizar.save();
+    })
+    .then(result => {
+      res.redirect('/ingresar');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
